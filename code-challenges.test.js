f540891d0b129f4a25dffc90907d09fe7f599c7b
Varelazi <ilenevarela03@gmail.com>
2023-09-17 18:52:56 -0700

    week three assessment done

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
+const { array } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -22,7 +24,38 @@ const fibonacciLength2 = 10
 
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+//Jest Testing: 
+// describe("fibSequence", () => {
+//   it("Returns an array that is the length of the given input. It adds the numbers inside the array, but in Fibonacci sequence", () => {
+//     expect(fibSequence(fibonacciLength1)).toEqual([1, 1, 2, 3, 5, 8])
+//     expect(fibSequence(fibonacciLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+//   })
+// })
+
+//Psuedocode
+//Function Name: fibSequence 
+//Input: Number
+//Output: An array with the length of the input number. 
+//Process: Turn given input into the length of the array. Once we have the length we will need to have 1 as the first value, 1 as the second value to add to 2. 2 as the third value to add to three and so on. An if statement to allow only numbers bigger than 2 to be added as the length of the array. If statement to see if number inputed is going to be greater than two, this will be the length of the array we are making. Make an array using Array(). A for loop to add numbers into array. Add logic inside for loop to make Fibonacci sequence work. 
+
+// const fibSequence = (num) => {
+//   if(num > 2){
+//     let arrayLength = Array(num)
+//     let startFibArray = [1,1]
+//     //Stuck on trying to make the logic of the for loop work. Make the fibonacci logic.
+//     for (let i = 2; i < arrayLength.length; i++) {
+//       // [1 + 1 + 2 + 3 + 5 + 8]
+//       startFibArray[i] = startFibArray[i-2]+startFibArray[i-1]
+//      }
+//     //Was able to add numbers into empty array by moving return out of the for loop. Kept adding it into for loop which would stop the loop after the one time.
+//     return startFibArray
+//   }
+// }
+// console.log(fibSequence(fibonacciLength1))
+// console.log(fibSequence(fibonacciLength2))
+//Cannot figure out the logic for the life of me.
+//Had to get some help to figure out the logic inside the for loop. 
+
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
@@ -53,7 +86,30 @@ const studyMinutesWeek2 = {
 
 // b) Create the function that makes the test pass.
 
+//Jest Testing:
+// describe("leastToGreatest", () => {
+//   it("Returns an array with the values sorted from least to greatest study minutes", () =>{
+//     expect(leastToGreatest(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+//     expect(leastToGreatest(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+//   })
+// })
+
 // Pseudo code:
+//Function Name: leastToGreatest
+//Input: Array with objects/key: value
+//Output: An array with JUST the values and it being ordered from small number to big number. 
+//Process: .sort() to organize each number from lowest to greatest. Object.value() to get the value of the object that is being given. Use the function (a,b) => a-b which will sort the numbers in ascending order even if there is a (100) triple digit number. 
+
+// const leastToGreatest = (object) => {
+//   const var1 = Object.values(object)
+//   Holds each array as: no key just value
+//   console.log(var1)
+//   const var2 = var1.sort((a,b) => a-b)
+//   return var2
+// }
+//Code sorts the numbers, but only issue is that 100 is on the second index instead of being last. 
+// console.log(leastToGreatest(studyMinutesWeek1))
+// console.log(leastToGreatest(studyMinutesWeek2))
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
@@ -70,4 +126,26 @@ const accountTransactions3 = []
 
 // b) Create the function that makes the test pass.
 
+//Jest Testing
+// describe("accumulatingSum", () => {
+//   it("Returns the accumulating sum of the array. If an empty array, then it should return an empty array.", () => {
+//     expect(accumulatingSum(accountTransactions1)).toEqual([100, 83, 60, 51])
+//     expect(accumulatingSum(accountTransactions2)).toEqual([250, 161, 261, 165])
+//     expect(accumulatingSum(accountTransactions3)).toEqual([])
+//   })
+// })
+
 // Pseudo code:
+//Function Name: accumulatingSum
+//Input: Array of numbers
+//Output: Array of accumulated sum of the numbers inside.
+//Process: .map() to create a new array with the given information. 
+
+const accumulatingSum = (arr) => {
+    let addSum = 0
+    return arr.map((value) => addSum += value)
+    //Not understanding how addSum += value works. Could you explain it. Please and thank you
+}
+console.log(accumulatingSum(accountTransactions1))
+
+//Feeling a bit disappointed that I couldn't solve these problems by myself. Had to get help from others. 
--- interview-questions.md
+++ interview-questions.md
@@ -6,42 +6,42 @@ First, without external resources. Challenge yourself to answer from memory as i
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
-Type your initials here to confirm you read the instructions:
+Type your initials here to confirm you read the instructions: IV
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX is a combination of JavaScript and HTML code. It allows us to combine both codes into one file. HTML uses the greater than less than syntaxs. I need to review the HTML coding langauge more because I do not remember the name of the HTML syntax, but it is shows like this: <> </>. To have JavaScript code you need to use {} curly brakets which lets the program know that whatever is inside the brakets is JavaScript code and not HTML.  
 
-Researched answer:
+Researched answer: After researching my answer I realized that I read the question wrong. JSX stands for JavaScript XML. It allows us to write HTML in React. The syntax they both use is the tag syntax. The differnece between the tag syntax in HTML and JSK would be that HTML does not have to return a single parent element while the JSK requires you to return a signle parent element. 
 
 2. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: I am not entierely sure what yarn is, but I do know that it works with JavaScript and React. It also works within the computers terminal to run code. All the files that you work on are modified after you work on them once you run the command in the terminal. 
 
-Researched answer:
+Researched answer: After researching the answer I learned that yarn is a package manager. It allows developers to share code around the world. Everytime a developer runs the yarn command in the terminal the files that are updated are the package-lock.json and yarn.lock files.
 
 3. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: In simple terms, states values allow you to change hard code. You are allowed to change a hard codded code. A prop allows you to transfer data into other files within the same React App files. 
 
-Researched answer:
+Researched answer: Props are used to pass data from one component to another compononent while state is used to manage data within the component it is called in. Props cannot change or be changed, but states can be updated using setState. 
 
 4. What is the DOM?
 
-Your answer:
+Your answer: DOM is an event in which allows user interaction within the app. 
 
-Researched answer:
+Researched answer: DOM, also known as Document Object Model is an important interface that allows interactions within the website. 
 
 5. STRETCH: Which is the difference between a div tag and a span tag?
 
-Your answer:
+Your answer: I don't know or remember what a span tag is, but a div tag stands for divide which will divide whatever data you have written between the tag syntax. 
 
-Researched answer:
+Researched answer: A span tag is used to group smaller pieces of text together. Div tags are used to group large pieces of text. 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: A programming style based on classes and objects. It groups data and methods. 
 
-2. Ruby:
+2. Ruby: A scripting langauge that doesn't talk directly to the hardware. It is designed for front and back end development. 
 
-3. Ruby blocks:
+3. Ruby blocks: Anonymous functions that can be passed into methods. Blocks are enclosed in a do / end statement or between {} brakets. They can hold multuple arguments. 
